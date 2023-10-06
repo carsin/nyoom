ca073ed01b24ec19d29e58a327259180cf806d19
carsin <20868514+carsin@users.noreply.github.com>
2023-10-06 11:23:44 +0200

    add 2 new tabs and give all tabs names

--- src/views/ProfilePage.vue
+++ src/views/ProfilePage.vue
@@ -2,17 +2,17 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 2</ion-title>
+        <ion-title>Profile</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Tab 2</ion-title>
+          <ion-title size="large">Profile</ion-title>
         </ion-toolbar>
       </ion-header>
 
-      <ExploreContainer name="Tab 2 page" />
+      <ExploreContainer name="Profile page" />
     </ion-content>
   </ion-page>
 </template>

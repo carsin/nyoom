202a8eab766348051c6b6991aab90647f8470a27
carsin <20868514+carsin@users.noreply.github.com>
2023-10-06 12:42:57 +0200

    add card component to feed

--- src/views/FeedPage.vue
+++ src/views/FeedPage.vue
@@ -1,23 +1,37 @@
 <template>
-  <ion-page>
-    <ion-header>
-      <ion-toolbar>
-        <ion-title>Feed</ion-title>
-      </ion-toolbar>
-    </ion-header>
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Feed</ion-title>
-        </ion-toolbar>
-      </ion-header>
+    <ion-page>
+        <ion-header>
+            <ion-toolbar>
+                <ion-title>Feed</ion-title>
+            </ion-toolbar>
+        </ion-header>
+        <ion-content :fullscreen="true">
+            <ion-header collapse="condense">
+                <ion-toolbar>
+                    <ion-title size="large">Feed</ion-title>
+                </ion-toolbar>
+            </ion-header>
 
-      <ExploreContainer name="Feed Page" />
-    </ion-content>
-  </ion-page>
+            <ExploreContainer name="Feed Page" />
+
+            <ion-card>
+                <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" />
+                <ion-card-header>
+                    <ion-card-title>Card Title</ion-card-title>
+                    <ion-card-subtitle>Card Subtitle</ion-card-subtitle>
+                </ion-card-header>
+                <ion-card-content>
+                    Here's a small text description for the card content. Nothing more, nothing less.
+                </ion-card-content>
+            </ion-card>
+
+        </ion-content>
+    </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
 import ExploreContainer from '@/components/ExploreContainer.vue';
+import { defineComponent } from 'vue';
+
 </script>

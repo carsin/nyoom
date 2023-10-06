158a1dddbb5dd3fe7ca42da3a826af5a619cd1ca
carsin <20868514+carsin@users.noreply.github.com>
2023-10-06 11:23:44 +0200

    add 2 new tabs and give all tabs names

--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -3,19 +3,29 @@
     <ion-tabs>
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
-        <ion-tab-button tab="tab1" href="/tabs/tab1">
-          <ion-icon aria-hidden="true" :icon="triangle" />
-          <ion-label>Tab 1</ion-label>
-        </ion-tab-button>
-
-        <ion-tab-button tab="tab2" href="/tabs/tab2">
-          <ion-icon aria-hidden="true" :icon="ellipse" />
-          <ion-label>Tab 2</ion-label>
-        </ion-tab-button>
-
-        <ion-tab-button tab="tab3" href="/tabs/tab3">
+        <ion-tab-button tab="tab1" href="/home">
           <ion-icon aria-hidden="true" :icon="square" />
-          <ion-label>Tab 3</ion-label>
+          <ion-label>Feed</ion-label>
+        </ion-tab-button>
+
+        <ion-tab-button tab="tab2" href="/search">
+          <ion-icon aria-hidden="true" :icon="square" />
+          <ion-label>Search</ion-label>
+        </ion-tab-button>
+
+        <ion-tab-button tab="tab3" href="/events">
+          <ion-icon aria-hidden="true" :icon="square" />
+          <ion-label>Events</ion-label>
+        </ion-tab-button>
+                
+        <ion-tab-button tab="tab4" href="/market">
+          <ion-icon aria-hidden="true" :icon="square" />
+          <ion-label>Market</ion-label>
+        </ion-tab-button>
+                
+        <ion-tab-button tab="tab5" href="/profile">
+          <ion-icon aria-hidden="true" :icon="square" />
+          <ion-label>My Profile</ion-label>
         </ion-tab-button>
       </ion-tab-bar>
     </ion-tabs>

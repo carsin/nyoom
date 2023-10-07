1258238a26657e76bdfabc03fffbbe5167f4b127
carsin <20868514+carsin@users.noreply.github.com>
2023-10-07 08:27:17 +0200

    Revert "infinite scroll feed"
    This reverts commit 92fbfb96be297b24581d34d602915dc56d6491e0.

--- src/views/FeedPage.vue
+++ src/views/FeedPage.vue
@@ -11,63 +11,27 @@
                     <ion-title size="large">Feed</ion-title>
                 </ion-toolbar>
             </ion-header>
-            <!-- INFINITE SCROLL OF POSTS -->
-            <ion-list>
-                <ion-item v-for="(item, index) in items">
-                    <ion-card>
-                        <img :src="'https://picsum.photos/300/300?random=' + index" alt="avatar" />
-                        <ion-card-header>
-                            <ion-card-title>Card Title</ion-card-title>
-                            <ion-card-subtitle>Card Subtitle</ion-card-subtitle>
-                        </ion-card-header>
-                        <ion-card-content>
-                            Here's a small text description for the card content. Nothing more, nothing less.
-                        </ion-card-content>
-                    </ion-card>
-                    <ion-label>{{ item }}</ion-label>
-                </ion-item>
-            </ion-list>
-            <ion-infinite-scroll @ionInfinite="ionInfinite">
-                <ion-infinite-scroll-content></ion-infinite-scroll-content>
-            </ion-infinite-scroll>
+
+            <ExploreContainer name="Feed Page" />
+
+            <ion-card>
+                <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" />
+                <ion-card-header>
+                    <ion-card-title>Card Title</ion-card-title>
+                    <ion-card-subtitle>Card Subtitle</ion-card-subtitle>
+                </ion-card-header>
+                <ion-card-content>
+                    Here's a small text description for the card content. Nothing more, nothing less.
+                </ion-card-content>
+            </ion-card>
+
         </ion-content>
     </ion-page>
 </template>
 
-<script lang="ts">
-    import {
-        IonContent, IonInfiniteScroll, IonInfiniteScrollContent, IonList, IonItem, IonAvatar, IonImg, IonLabel,
-        InfiniteScrollCustomEvent, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonPage, IonHeader, IonToolbar, IonTitle,
-    } from '@ionic/vue';
-  
-    import { defineComponent, reactive } from 'vue';
-    export default defineComponent({
-        components: {
-            IonContent,
-            IonInfiniteScroll,
-            IonInfiniteScrollContent,
-            IonList,
-            IonItem,
-            IonImg,
-            IonLabel,
-        },
-        setup() {
-            const items = reactive([]);
+<script setup lang="ts">
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import ExploreContainer from '@/components/ExploreContainer.vue';
+import { defineComponent } from 'vue';
 
-            const generateItems = () => { // add more item logic
-                const count = items.length + 1;
-                for (let i = 0; i < 50; i++) {
-                    items.push(`Item ${count + i}`); // dunno why this gives off an error
-                }
-            };
-
-            const ionInfinite = (ev: InfiniteScrollCustomEvent) => {
-                generateItems();
-                setTimeout(() => ev.target.complete(), 500);
-            };
-
-            generateItems();
-            return { ionInfinite, items };
-        },
-    });
 </script>

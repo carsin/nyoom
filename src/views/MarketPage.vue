123a9d373baf1ff03a7e3d6ba681ca864459d94d
carsin <20868514+carsin@users.noreply.github.com>
2023-10-09 08:53:40 +0200

    infinite feed scroll breaks search and market pages

--- src/views/MarketPage.vue
+++ src/views/MarketPage.vue
@@ -161,7 +161,7 @@
   </ion-page>
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
 import ExploreContainer from '@/components/ExploreContainer.vue';
 </script>
--- src/views/ProfilePage.vue
+++ src/views/ProfilePage.vue
@@ -8,7 +8,7 @@
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Profile</ion-title>
+          <ion-title size="large">Profileheader</ion-title>
         </ion-toolbar>
       </ion-header>
 

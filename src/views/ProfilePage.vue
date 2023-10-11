6229c3ebd4a13175c19fd5187e011f76e3cf9ed9
carsin <20868514+carsin@users.noreply.github.com>
2023-10-11 22:00:40 +0200

    fix profile header positioning and sources/ -> assets/

--- src/views/ProfilePage.vue
+++ src/views/ProfilePage.vue
@@ -1,92 +1,77 @@
 <template>
   <ion-page>
-    <!-- <ion-header>
-      <ion-toolbar>
-        <ion-buttons slot="start">
-          <ion-button>
-            <ion-icon slot="icon-only" :icon="settingsSharp"></ion-icon>
-          </ion-button>
-        </ion-buttons>
-        <ion-buttons slot="end">
-          <ion-button>
-            <ion-icon slot="icon-only" :icon="peopleSharp"></ion-icon>
-          </ion-button>
-          <ion-button>
-            <ion-icon slot="icon-only" :icon="carSportSharp"></ion-icon>
-          </ion-button>
-        </ion-buttons>
-        <ion-title align="center">@username</ion-title>
-      </ion-toolbar>
-      <ion-toolbar>
-        <ion-button fill="clear">
-          <ion-img src="src\sources\modified-race-car.jpg"></ion-img>
-        </ion-button>
-      </ion-toolbar>
-      <ion-toolbar>
-        <ion-title align="center" size="small">here is where the bio goes. There will be a word/letter limit on the bio in the future.</ion-title>
-      </ion-toolbar>
-    </ion-header> -->
-
     <ion-content :fullscreen="true">
       <ion-toolbar>
-        <ion-buttons slot="start">
-          <ion-button>
-            <ion-icon slot="icon-only" :icon="settingsSharp"></ion-icon>
-          </ion-button>
-        </ion-buttons>
-        <ion-buttons slot="end">
-          <ion-button>
-            <ion-icon slot="icon-only" :icon="peopleSharp"></ion-icon>
-          </ion-button>
-          <ion-button>
-            <ion-icon slot="icon-only" :icon="carSportSharp"></ion-icon>
-          </ion-button>
-        </ion-buttons>
-        <ion-title align="center">@username</ion-title>
-      </ion-toolbar>
-      <ion-toolbar>
-        <ion-button fill="clear">
-          <ion-img src="src\sources\modified-race-car.jpg"></ion-img>
-        </ion-button>
-      </ion-toolbar>
-      <ion-toolbar>
-        <ion-title align="center" size="small">here is where the bio goes. There will be a word/letter limit on the bio in the future.</ion-title>
+        <ion-grid>
+          <ion-row>
+            <ion-col size="1">
+              <ion-buttons>
+                <ion-button>
+                  <ion-icon slot="icon-only" :icon="settingsSharp"></ion-icon>
+                </ion-button>
+              </ion-buttons>
+            </ion-col>
+            <ion-col class="ion-text-center" size="10">
+              <ion-title align="center">@username</ion-title>
+              <img id="profile-avatar" src="/src/assets/modified-race-car.jpg"/>
+            </ion-col>
+            <ion-col size="1">
+              <ion-buttons class="ion-float-right">
+                <ion-list>
+                  <ion-item>
+                    <ion-button>
+                      <ion-icon slot="icon-only" :icon="peopleSharp"></ion-icon>
+                    </ion-button>
+                  </ion-item>
+                  <ion-item>
+                    <ion-button>
+                      <ion-icon slot="icon-only" :icon="carSportSharp"></ion-icon>
+                    </ion-button>
+                  </ion-item>
+                </ion-list>
+              </ion-buttons>
+            </ion-col>
+          </ion-row>
+          <ion-row class="ion-text-center">
+            <ion-title size="small">here is where the bio goes. There will be a word/letter limit on the bio in the future.</ion-title>
+          </ion-row>
+        </ion-grid>
       </ion-toolbar>
       <ion-list>
         <ion-item>
           <ion-card>
-              <img alt="Part image" src="https://ionicframework.com/docs/img/demos/card-media.png"/>
-              <ion-card-header>
-                <ion-card-title>Car #1</ion-card-title>
-                <ion-card-subtitle>Car Description</ion-card-subtitle>
-              </ion-card-header>
+            <img alt="Part image" src="https://ionicframework.com/docs/img/demos/card-media.png" />
+            <ion-card-header>
+              <ion-card-title>Car #1</ion-card-title>
+              <ion-card-subtitle>Car Description</ion-card-subtitle>
+            </ion-card-header>
           </ion-card>
         </ion-item>
         <ion-item>
           <ion-card>
-              <img alt="Part image" src="https://ionicframework.com/docs/img/demos/card-media.png"/>
-              <ion-card-header>
-                <ion-card-title>Car #2</ion-card-title>
-                <ion-card-subtitle>Car Description</ion-card-subtitle>
-              </ion-card-header>
+            <img alt="Part image" src="https://ionicframework.com/docs/img/demos/card-media.png" />
+            <ion-card-header>
+              <ion-card-title>Car #2</ion-card-title>
+              <ion-card-subtitle>Car Description</ion-card-subtitle>
+            </ion-card-header>
           </ion-card>
         </ion-item>
         <ion-item>
           <ion-card>
-              <img alt="Part image" src="https://ionicframework.com/docs/img/demos/card-media.png"/>
-              <ion-card-header>
-                <ion-card-title>Car #3</ion-card-title>
-                <ion-card-subtitle>Car Description</ion-card-subtitle>
-              </ion-card-header>
+            <img alt="Part image" src="https://ionicframework.com/docs/img/demos/card-media.png" />
+            <ion-card-header>
+              <ion-card-title>Car #3</ion-card-title>
+              <ion-card-subtitle>Car Description</ion-card-subtitle>
+            </ion-card-header>
           </ion-card>
         </ion-item>
         <ion-item>
           <ion-card>
-              <img alt="Part image" src="https://ionicframework.com/docs/img/demos/card-media.png"/>
-              <ion-card-header>
-                <ion-card-title>Car #4</ion-card-title>
-                <ion-card-subtitle>Part Description</ion-card-subtitle>
-              </ion-card-header>
+            <img alt="Part image" src="https://ionicframework.com/docs/img/demos/card-media.png" />
+            <ion-card-header>
+              <ion-card-title>Car #4</ion-card-title>
+              <ion-card-subtitle>Part Description</ion-card-subtitle>
+            </ion-card-header>
           </ion-card>
         </ion-item>
       </ion-list>
@@ -94,11 +79,20 @@
   </ion-page>
 </template>
 
+<style>
+  #profile-avatar {
+    border-radius: 50%;
+    width: 100px;
+    height: 100px;
+    object-fit: cover;
+  }
+</style>
+
 <script lang="ts">
-import { IonGrid, IonRow, IonCol, IonIcon, IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, IonList, IonCard, IonItem, IonImg } from '@ionic/vue';
-import { settingsSharp, peopleSharp, carSportSharp } from 'ionicons/icons';
-import { defineComponent } from 'vue';
-export default defineComponent({
+  import { IonAvatar, IonGrid, IonRow, IonCol, IonIcon, IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, IonList, IonCard, IonItem, IonImg } from '@ionic/vue';
+  import { settingsSharp, peopleSharp, carSportSharp } from 'ionicons/icons';
+  import { defineComponent } from 'vue';
+  export default defineComponent({
     components: { IonButton, IonButtons, IonIcon, IonTitle, IonToolbar, IonList, IonItem, IonCard, IonImg, IonGrid, IonRow, IonCol },
     setup() {
       return { settingsSharp, peopleSharp, carSportSharp };

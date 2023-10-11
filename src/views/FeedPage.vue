501a9e6ce4569a26045fd44b260910bb5f759b1b
carsin <20868514+carsin@users.noreply.github.com>
2023-10-11 20:35:50 +0200

    feed prototype complete using FeedCardComponent

--- src/views/FeedPage.vue
+++ src/views/FeedPage.vue
@@ -11,23 +11,17 @@
                     <ion-title size="large">Feed</ion-title>
                 </ion-toolbar>
             </ion-header>
-
-            <ExploreContainer name="Feed Page" />
-
-            <ion-card>
-                <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" />
-                <ion-card-header>
-                    <ion-card-title>Card Title</ion-card-title>
-                    <ion-card-subtitle>Card Subtitle</ion-card-subtitle>
-                </ion-card-header>
-                <ion-card-content>
-                    Here's a small text description for the card content. Nothing more, nothing less.
-                </ion-card-content>
-            </ion-card>
+      
+        <FeedCardComponent username="@testmanposting1993" caption="Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis." upvotes="23563" downvotes="230" image_src="../assets/carpic1.png"/>
+        <FeedCardComponent username="@justanotherpostah42" caption="Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat." upvotes="2832" downvotes="91" image_src="../assets/carpic2.png"/>
+        <FeedCardComponent username="@thisguylovesposting1973" caption="Caption blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah " upvotes="2822" downvotes="232" image_src="../assets/carpic3.png"/>
+        <FeedCardComponent username="@thisguylovesposting1973" caption="Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis." upvotes="9232" downvotes="822" image_src="../assets/carpic4.png"/>
+        <FeedCardComponent username="@thisguylovesposting1973" caption="Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis." upvotes="10203" downvotes="920" image_src="../assets/carpic5.png"/>
         </ion-content>
     </ion-page>
 </template>
 
 <script setup lang="ts">
 import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import FeedCardComponent from '@/components/FeedCardComponent.vue';
 </script>

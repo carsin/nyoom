c0393625a77c4afab7354bd2082edc9b9dee75b2
carsin <20868514+carsin@users.noreply.github.com>
2023-10-23 16:38:12 +0200

    integrate firebase setup

--- src/App.vue
+++ src/App.vue
@@ -6,4 +6,13 @@
 
 <script setup lang="ts">
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
+import { firebaseAuth } from "./firebase-service";
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  // Change the title when the component is mounted
+  document.title = 'Nyoom';
+});
+console.log(firebaseAuth);
+
 </script>

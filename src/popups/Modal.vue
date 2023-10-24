9630e124d3b8590c3a069da150ff3b23672e5b25
Diana Koralski <dianakoralski@hotmail.com>
2023-10-24 16:06:06 +0200

    Parts Filter Modal

---
+++ src/popups/Modal.vue
@@ -0,0 +1,54 @@
+<template>
+  <ion-header>
+    <ion-toolbar>
+      <ion-buttons slot="start">
+        <ion-button color="medium" @click="cancel">Cancel</ion-button>
+      </ion-buttons>
+      <ion-title>Filter Parts</ion-title>
+      <ion-buttons slot="end">
+        <ion-button @click="confirm" :strong="true">Confirm</ion-button>
+      </ion-buttons>
+    </ion-toolbar>
+  </ion-header>
+  <ion-content class="ion-padding">
+    <ion-list>
+      <ion-item :class="{ 'selected-option': selectedOption === 'Featured' }" @click="selectOption('Featured')">Featured</ion-item>
+      <ion-item :class="{ 'selected-option': selectedOption === 'Price: Low to High' }" @click="selectOption('Price: Low to High')">Price: Low to High</ion-item>
+      <ion-item :class="{ 'selected-option': selectedOption === 'Price: Hight to Low' }" @click="selectOption('Price: Hight to Low')">Price: Hight to Low</ion-item>
+      <ion-item :class="{ 'selected-option': selectedOption === 'Distance: Closest to Furthest' }" @click="selectOption('Distance: Closest to Furthest')">Distance: Closest to Furthest</ion-item>
+
+    </ion-list>
+  </ion-content>
+</template>
+
+<script lang="ts" setup>
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonButtons,
+  IonButton,
+  IonList,
+  IonItem,
+  modalController,
+} from '@ionic/vue';
+import { defineComponent, ref } from 'vue';
+
+const name = ref();
+const selectedOption = ref('');
+
+const cancel = () => modalController.dismiss(null, 'cancel');
+const confirm = () => modalController.dismiss(selectedOption.value, 'confirm');
+
+const selectOption = (option) => {
+  selectedOption.value = option;
+};
+</script>
+
+<style scoped>
+.selected-option {
+  background-color: #0078d4; /* Change the background color when selected */
+  color: #00ff51; /* Change the text color when selected */
+}
+</style>
--- src/views/MarketPage.vue
+++ src/views/MarketPage.vue
@@ -2,7 +2,7 @@
   <ion-page>
     <ion-header>
       <ion-toolbar collapse="condense">
-        <ion-title >Market</ion-title>
+        <ion-title>Market</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
@@ -21,23 +21,39 @@
           <ion-title class="ion-text-center">Local Parts</ion-title>
         </ion-toolbar>
         <ion-toolbar>
-          <ion-searchbar
-            show-clear-button="focus"
-            placeholder="Search parts"
-          ></ion-searchbar>
-        </ion-toolbar>
-        
+  <ion-searchbar
+    show-clear-button="focus"
+    placeholder="Search parts"
+    class="ion-padding-start ion-padding-end"
+  ></ion-searchbar>
+  <ion-buttons slot="end">
+  <ion-button expand="block" @click="openModal">
+  <ion-icon :icon="funnel" />
+</ion-button>
+</ion-buttons>
+<p>{{ message }}</p>
+</ion-toolbar>
+
+
         <ion-grid>
           <ion-row>
             <ion-col v-for="part in parts" :key="part.id" size="6">
               <ion-card :href="'/market/' + part.id" class="custom-card">
-  <img alt="Part image" :src="part.imageUrl" class="custom-image"/>
-  <ion-card-header>
-    <ion-card-subtitle>{{ part.condition }}</ion-card-subtitle>
-    <ion-card-subtitle class="card-price">{{ part.price }}</ion-card-subtitle>
-    <ion-card-subtitle class="card-title">{{ part.title }}</ion-card-subtitle>
-  </ion-card-header>
-</ion-card>
+                <img
+                  alt="Part image"
+                  :src="part.imageUrl"
+                  class="custom-image"
+                />
+                <ion-card-header>
+                  <ion-card-subtitle>{{ part.condition }}</ion-card-subtitle>
+                  <ion-card-subtitle class="card-price">{{
+                    part.price
+                  }}</ion-card-subtitle>
+                  <ion-card-subtitle class="card-title">{{
+                    part.title
+                  }}</ion-card-subtitle>
+                </ion-card-header>
+              </ion-card>
             </ion-col>
           </ion-row>
         </ion-grid>
@@ -54,15 +70,24 @@
             placeholder="Search deals"
           ></ion-searchbar>
         </ion-toolbar>
-        
+
         <ion-grid>
           <ion-row>
             <ion-col v-for="offer in offers" :key="offer.id" size="6">
-              <ion-card :href="'/market/autoshop/' + offer.id" class="custom-card">
-                <img alt="Offer image" :src="offer.imageUrl" class="custom-image" />
+              <ion-card
+                :href="'/market/autoshop/' + offer.id"
+                class="custom-card"
+              >
+                <img
+                  alt="Offer image"
+                  :src="offer.imageUrl"
+                  class="custom-image"
+                />
                 <ion-card-header>
                   <ion-card-subtitle>{{ offer.deal }}</ion-card-subtitle>
-                  <ion-card-title class="card-title">{{ offer.shopName }}</ion-card-title>
+                  <ion-card-title class="card-title">{{
+                    offer.shopName
+                  }}</ion-card-title>
                 </ion-card-header>
               </ion-card>
             </ion-col>
@@ -75,9 +100,9 @@
 
 <style scoped>
 .custom-card {
-  height: 250px; 
+  height: 250px;
   margin: 0px;
-  overflow: hidden; 
+  overflow: hidden;
 }
 .custom-image {
   height: 100px;
@@ -88,13 +113,13 @@
   font-size: 16px;
   font-weight: "bold";
   max-width: 200px;
-  overflow: hidden; 
+  overflow: hidden;
 }
 .card-price {
   color: green;
   font-weight: "bold";
   max-width: 200px;
-  overflow: hidden; 
+  overflow: hidden;
 }
 </style>
 
@@ -115,6 +140,45 @@ const selectTab = (tab: String) => {
 const isPartsTab = computed(() => selectedTab.value === "parts");
 const isAutoShopTab = computed(() => selectedTab.value === "auto-shop");
 
-import { IonLabel, IonSegment, IonCardTitle, IonCard, IonSegmentButton, IonGrid, IonRow, IonCol, IonCardSubtitle, IonPage, IonToolbar, IonTitle, IonContent, IonCardHeader, IonSearchbar, IonButton } from '@ionic/vue';
-</script>
+import Modal from "../popups/Modal.vue";
+import { ref } from "vue";
 
+const message = ref(
+  "Sort by: Featured (default)"
+);
+
+const openModal = async () => {
+  const modal = await modalController.create({
+    component: Modal,
+  });
+
+  modal.present();
+
+  const { data, role } = await modal.onWillDismiss();
+
+  if (role === "confirm") {
+    message.value = `Sort by: ${data}`;
+  }
+};
+
+import {
+  IonHeader,
+  modalController,
+  IonSegment,
+  IonCardTitle,
+  IonCard,
+  IonSegmentButton,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonCardSubtitle,
+  IonPage,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonCardHeader,
+  IonSearchbar,
+  IonButton,
+} from "@ionic/vue";
+import { funnel } from "ionicons/icons";
+</script>
